c995f88b0e87d0b773c6cb76cd5a3c205faa6a3c
Andrew Bailey <Andrew@MacBook-Pro.local>
2018-11-13 14:17:57 -0600

    Initial Commit

---
+++ DemoFoursquare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoFoursquare
+//
+//  Created by Andrew Bailey on 11/13/18.
+//  Copyright © 2018 Andrew Bailey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
